2f8d1ef2b0b15dd3f346a1e224759308764b0d68
SABRE17\Martina <10230260c@gmail.com>
2024-03-18 05:10:14 +0800

    modify "ask for api"

--- src/views/chartView.vue
+++ src/views/chartView.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="chart">
     <h1>chart page</h1>
-    <!-- <h2>{{data.responseData}}</h2> -->
     <table>
-      <caption>嬰兒出生數按性別、生母原屬國籍（地區）、年齡及教育程度分(按登記)</caption>
-      <th>hello</th>
+      <th colspan="8">嬰兒出生數按性別、生母原屬國籍（地區）、年齡及教育程度分（按登記）</th>
       <tbody>
+        <tr>
+          <td>統計年</td>
+          <td>按照別</td>
+          <td>區域別</td>
+          <td>出生者性別</td>
+          <td>生母原屬國籍或地區</td>
+          <td>生母年齡</td>
+          <td>生母教育程度</td>
+          <td>嬰兒出生數</td>
+        </tr>
         <tr v-for="(item, index) in data" :key="index">
-          <td></td>
+          <td >{{ item.statistic_yyy }}</td>
+          <td >{{ item.according }}</td>
+          <td >{{ item.site_id }}</td>
+          <td >{{ item.birth_sex }}</td>
+          <td >{{ item.mother_nation }}</td>
+          <td >{{ item.mother_age}}</td>
+          <td >{{ item.mother_education}}</td>
+          <td >{{ item.birth_count}}</td>
         </tr>
       </tbody>
-      <tfoot>2</tfoot>
+      <tfoot></tfoot>
     </table>
   </div>
 </template>
@@ -26,22 +41,19 @@ const data = ref("");
 
 function infoGet() {
   axios
-    .get('https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP028/108', {})
+    .get('https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP028/109', {})
     .then(res => {
       console.log(res.data);
-      data.value = res.data.responseData.slice(0,10);
+      data.value = res.data.responseData.slice(0,50);
     })
     .catch(error => console.log('錯誤', error));
 }
 onMounted(() => {
   infoGet();
-  // console.log(infoGet());
   });
 
 return {
   data,
   infoGet,
 };}}
-
-
 </script>
