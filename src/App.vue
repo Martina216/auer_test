ee5d3300d9fd53095fe5f1353199d7968b2adcd8
SABRE17\Martina <10230260c@gmail.com>
2024-03-18 03:16:24 +0800

    build Vue.js, SASS env

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import { onMounted } from 'vue';
+import chatBot from './components/ChatBot.vue';
+
+  // 執行於元件被掛載後
+  onMounted(() => {
+    console.log('Component mounted');
+  });
+
+  
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <nav>
+        <RouterLink to="/chart">圖表</RouterLink>
+        <RouterLink to="/forms">報表</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <chatBot/>
+
+  <RouterView />
+</template>
---
+++ src/views/chartView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="chart">
+    <h1>chart page</h1>
+    <!-- <h2>{{data.responseData}}</h2> -->
+    <table>
+      <caption>嬰兒出生數按性別、生母原屬國籍（地區）、年齡及教育程度分(按登記)</caption>
+      <th>hello</th>
+      <tbody>
+        <tr v-for="(item, index) in data" :key="index">
+          <td></td>
+        </tr>
+      </tbody>
+      <tfoot>2</tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import { ref,onMounted } from 'vue';
+import axios from 'axios';
+
+
+export default {
+  setup() {
+const data = ref("");
+
+function infoGet() {
+  axios
+    .get('https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP028/108', {})
+    .then(res => {
+      console.log(res.data);
+      data.value = res.data.responseData.slice(0,10);
+    })
+    .catch(error => console.log('錯誤', error));
+}
+onMounted(() => {
+  infoGet();
+  // console.log(infoGet());
+  });
+
+return {
+  data,
+  infoGet,
+};}}
+
+
+</script>
